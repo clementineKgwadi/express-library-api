bac8cf6d1caabf17c211843b8c73fa9b6cacfa25
Clementine <clementinekgwadi43@gmail.com>
2025-09-11 01:54:09 +0200

    add JSON parsing, logger middleware, and mount authors router

---
+++ src/routes/author.ts
@@ -0,0 +1,37 @@
+import { Router, Request, Response } from "express";
+import { body, param, validationResult } from "express-validator";
+import { getAllAuthors, getAuthorById, addAuthor } from "../controllers/author";
+
+const router = Router();
+
+router.get("/", getAllAuthors);
+
+router.get("/:id", 
+    [param("id").isInt().withMessage("Id must be an integer")],
+    (req:Request, res:Response) => {
+
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        getAuthorById(req, res);
+    }
+);
+
+router.post("/",
+    [body("name").notEmpty().withMessage("Author name is required")],
+    (req:Request, res:Response) => {
+
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json( {errors: errors.array()} )
+        }
+
+        addAuthor(req, res);
+    }
+)
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
+import bodyParser from 'body-parser';
 import express, {Express} from 'express'
+import { loggerMiddleware } from './middleware/logger';
+import router from './routes/author';
 
 const app: Express = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(loggerMiddleware);
+app.use("/authors", router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
