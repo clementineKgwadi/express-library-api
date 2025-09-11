ad573f3a31e3852fb5207c82a79cc21bff40026f
Clementine <clementinekgwadi43@gmail.com>
2025-09-11 19:54:09 +0200

    add updateAuthor and deleteAuthor controller functions

--- src/controllers/author.ts
+++ src/controllers/author.ts
@@ -18,7 +18,7 @@ export const getAuthorById = (req:Request, res:Response) => {
   })
 
   if(!author){
-    return res.status(404).send({ error: "Author not Found" })
+    return res.status(404).json({ error: "Author not Found" })
   }
   res.status(200).json(author)
 }
@@ -31,3 +31,31 @@ export const addAuthor = (req:Request, res:Response) => {
 
         res.status(201).json(newAuthor);
 }
+
+export const updateAuthor = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const author = authors.find(u => u.id === parseInt(id));
+
+  if (!author) {
+    return res.status(404).json({ error: "Author not Found" });
+  }
+
+  if (name) author.name = name;
+
+  res.status(200).json(author);
+};
+
+export const deleteAuthor = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const index = authors.findIndex(a => a.id === parseInt(id));
+
+  if (index === -1) {
+    return res.status(404).json({ error: "Author not found" });
+  }
+
+  const deletedAuthor = authors.splice(index, 1);
+
+  res.status(200).json({ message: "Author successfully deleted"});
+};
